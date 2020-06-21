4326119eab5367196e4227f0eaf1fb4292d3d646
Glitch (vr-builder) <none>
2020-06-21 15:25:12 +0000

    🍱🍖 Checkpoint ./public/js/stockist.js:69342/2167

--- public/js/stockist.js
+++ public/js/stockist.js
@@ -1,10 +1,16 @@
 /* global AFRAME */
+
+let addPrimitives = function($parent, color) {
+  $parent.append($(`<a-sphere class="stock touchable" cloneable position="0 0.1 0" scale="0.03 0.03 0.03" color="${color}">`))
+  $parent.append($(`<a-box class="stock touchable" cloneable position="0 0 0" scale="0.06 0.06 0.06" color="${color}">`))
+  $parent.append($(`<a-cylinder class="stock touchable" cloneable position="0 -0.1 0" scale="0.03 0.06 0.03" rotation="90 0 0" color="${color}">`))  
+}
+
 AFRAME.registerComponent('stockist', {
   init: function() {
     let $self = $(this.el)
-    $self.append($('<a-sphere class="stock touchable" cloneable position="0 0.1 -0.1" scale="0.03 0.03 0.03" color="green">'))
-    $self.append($('<a-box class="stock touchable" cloneable position="0 0 -0.1" scale="0.06 0.06 0.06" color="green">'))
-    $self.append($('<a-cylinder class="stock touchable" cloneable position="0 -0.1 -0.1" scale="0.03 0.06 0.03" rotation="90 0 0" color="green">'))
+    let $greens = $('<a-entity position="0 0 -0.2" scale="0.4 0.4 0.4">').appendTo($self)
+    addPrimitives($greens, 'green')
     $self.append($('<a-box monitor cloneable position="0 0 0.2" rotation="0 -90 90" scale="0.2 0.2 0.02" color="white">'))
   }
 });
