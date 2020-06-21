4b48e18f75992b84afca6ab59214d21b38c3f1c8
Glitch (vr-builder) <none>
2020-06-21 23:31:30 +0000

    👻🐮 Checkpoint ./public/js/resizer.js:69342/885

--- public/js/resizer.js
+++ public/js/resizer.js
@@ -4,14 +4,27 @@ AFRAME.registerComponent('resizer', {
   init: function() {
     let self = this
     let host = self.el
+    let 
     setTimeout(() => {
       let grabber = host.components.grabber
       if (!grabber) {
         clog('oops - resizer needs a grabber on this element!!')
         return
       }
-      grabber.onSecondGrab((grabbed) => {
+      grabber.onSecondGrab((grabbed, otherGrabber) => {
         clog('resizer', 'in resizer i got a second grab event for: ' + grabbed.tagName)
+        
+        let grabDistance = distanceBetween(self.el, otherGrabber)
+        let resizeFactor = grabDistance / self.resizeInitialDistance
+
+        // clog(`grabDistance ${grabDistance} initial ${self.resizeInitialDistance}`)
+
+        let posInGrabbedSpace = positionRelativeTo(parent, self.grabbed)
+        // clog(`resizer pos in space of grabbed:\n${posInGrabbedSpace.x.toFixed(2)} ${posInGrabbedSpace.y.toFixed(2)} ${posInGrabbedSpace.z.toFixed(2)}`)
+        self.resizingComponent.updateInfo({'resizer pos': xyzToFixed(posInGrabbedSpace)})
+        let newScale = self.resizeInitialScale.clone().multiplyVectors(self.resizeInitialScale, getResizeVector(resizeFactor, posInGrabbedSpace))
+        self.grabbed.object3D.scale.copy(newScale) 
+
       })      
     }, 0)
   }
