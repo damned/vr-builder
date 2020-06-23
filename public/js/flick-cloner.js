a9c595eefc627dcfe3279136028f9ac0e815eff0
Glitch (vr-builder) <none>
2020-06-23 22:11:29 +0000

    😽🚝 Checkpoint ./public/js/flick-cloner.js:69342/841

--- public/js/flick-cloner.js
+++ public/js/flick-cloner.js
@@ -1,11 +1,23 @@
-/* global AFRAME clog */
+/* global AFRAME THREE clog */
 AFRAME.registerComponent('flick-cloner', {
   init: function() {
     let self = this
+    let object3d = self.el.object3D
     const TIME_TO_STOP_MS = 100
-    const MIN_PRE_FLICK_VELOCITY = 
-    self.tick = function() {
-      clog('flick-cloner', 'i am watching')
+    const MIN_PRE_FLICK_VELOCITY = 1
+    const MAX_STOP_VELOCITY = 0.1
+    let lastPos
+    let nowPos
+    self.tick = function(time, timeDelta) {
+      object3d.getWorldPosition(nowPos)
+      if (lastPos) {
+        let velocity = 1000 * nowPos.distanceTo(lastPos) / timeDelta
+      clog('flick-cloner', 'i am watching')        
+      }
+      else {
+        lastPos = new THREE.Vector3()
+      }
+      lastPos.copy(nowPos)
     }
   }
 })
