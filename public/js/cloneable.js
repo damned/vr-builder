12826614339798279d2a4198d447bece9e31d344
Glitch (vr-builder) <none>
2020-06-23 23:34:24 +0000

    👆🚝 Checkpoint ./public/js/cloneable.js:69342/589

--- public/js/cloneable.js
+++ public/js/cloneable.js
@@ -1,19 +1,23 @@
-/* global AFRAME clog */
+/* global AFRAME clog catching */
 
 function copyableComponents(el) {
-  return el.components.filter(component => component.copyTo)
+  return Object.values(el.components).filter(component => component.copyTo)
 }
 
 function cloneEntity(entity) {
   entity.flushToDOM()
   let clone = entity.cloneNode()
+  entity.setAttribute('color', 'white')
   clone.removeAttribute('cloneable')
   document.getElementById('spawn').appendChild(clone)
-  setTimeout(() => 
-    copyableComponents(entity).forEach((sourceComponent) => {
-      clog('copying component', sourceComponent.name)
-      sourceComponent.copyTo(clone.components[sourceComponent.name]) 
-    }), 0)
+  setTimeout(() => {
+    catching(() => {
+      copyableComponents(entity).forEach((sourceComponent) => {
+        clog('copying component', sourceComponent.name)
+        sourceComponent.copyTo(clone.components[sourceComponent.name]) 
+      })
+    })
+  }, 0)
   return clone  
 }
 
