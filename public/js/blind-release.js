b67851d1f291526c8e7ebc9cb80dbdc574efda42
Glitch (vr-builder) <none>
2020-06-29 20:59:53 +0000

    raising 'blind-release' events if behind camera

--- public/js/blind-release.js
+++ public/js/blind-release.js
@@ -5,9 +5,16 @@ AFRAME.registerComponent('blind-release', {
     let self = this
     let host = self.el
     host.addEventListener('release', function(event) {
-      clog('blind-release', 'got release')
-      clog('blind-release', 'host position', this.object3D.getWorldPosition(new THREE.Vector3()))
-      clog('blind-release', 'relative position', positionRelativeTo(event.target, host.sceneEl.camera))
+      let released = event.target
+      let releasedPositionRelativeToCamera = positionRelativeTo(event.target, host.sceneEl.camera)
+      clog('blind-release', 'camera-relative position', releasedPositionRelativeToCamera)
+      if (releasedPositionRelativeToCamera.z > 0) {
+        clog('blind-release', 'was a blind release')
+        host.emit('blind-release', event)
+      }
+      else {
+        clog('blind-release', 'was not a blind release')
+      }
     })
   }
 })
