488e78a3a74e7b98f41ca4764887e30f51ac29d2
Glitch (vr-builder) <none>
2020-07-01 17:43:38 +0000

    🎬🔮 Checkpoint ./public/js/color-tweaker.js:69342/2547

--- public/js/color-tweaker.js
+++ public/js/color-tweaker.js
@@ -1,14 +1,32 @@
 /* global AFRAME colorFromEntityRotation */
 
+function findTouchSourceWeAreAttachedTo($host) {
+  let $touchSourceAncestor = $host.closest('[touch-source]')
+  if ($touchSourceAncestor.length == 0) {
+    return undefined
+  }
+  return $touchSourceAncestor.get(0).components['touch-source']
+}
+
 AFRAME.registerComponent('color-tweaker', {
   init: function() {
     let host = self.el
     let $host = $(host)
     $host.append(`<a-box class="color-tweaker-bounds" opacity="0.1" color="white"></a-box>`)
-    $host.append(`<a-sphere class="color-tweaker-bounds" opacity="0.1" color="white"></a-sphere>`)
-    $host.append(`<a-cylinder position="1.2 0 0" rotation="0 0 90" radius="0.5" height="0.3" color="red">`)
-    $host.append(`<a-cylinder position="0 1.2 0" rotation="0 0 0" radius="0.5" height="0.3" color="green">`)
-    $host.append(`<a-cylinder position="0 0 1.2" rotation="90 0 0" radius="0.5" height="0.3" color="blue">`)
+    let $model = $(`<a-sphere class="color-tweaker-model" radius="0.5"></a-sphere>`).appendTo($host)
+    $model.append(`<a-cylinder position="1.2 0 0" rotation="0 0 90" radius="0.5" height="0.3" color="red">`)
+    $model.append(`<a-cylinder position="0 1.2 0" rotation="0 0 0" radius="0.5" height="0.3" color="green">`)
+    $model.append(`<a-cylinder position="0 0 1.2" rotation="90 0 0" radius="0.5" height="0.3" color="blue">`)
+
+    let touchSource = findTouchSourceWeAreAttachedTo($host)
+    if (touchSource) {
+      touchSource.onTouchStart((touched) => {
+        clog('touch', 'in monitor i got a touch start for: ' + touched.tagName)
+        self.monitor(touched)
+      })
+    }
+    
+    
     self.tick = () => {
       host.setAttribute('color', colorFromEntityRotation(host))
     }
