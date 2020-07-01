581b71f2d0a4b33851f55886028d2cc4b78ab864
Glitch (vr-builder) <none>
2020-07-01 17:32:22 +0000

    👄📼 Checkpoint ./todo.txt:69342/125 ./public/js/color-tweaker.js:69342/873

--- public/js/color-tweaker.js
+++ public/js/color-tweaker.js
@@ -2,14 +2,16 @@
 
 AFRAME.registerComponent('color-tweaker', {
   init: function() {
-    this.el.setAttribute('follower', 'lock: position; leader: #left-hand')
-    let $self = $(this.el)
-    $self.append(`<a-cylinder position="1.2 0 0" rotation="0 0 90" radius="0.5" height="0.3" color="red">`)
-    $self.append(`<a-cylinder position="0 1.2 0" rotation="0 0 0" radius="0.5" height="0.3" color="green">`)
-    $self.append(`<a-cylinder position="0 0 1.2" rotation="90 0 0" radius="0.5" height="0.3" color="blue">`)
-  },
-  tick: function() {
-    this.el.setAttribute('color', colorFromEntityRotation(this.el))
+    let host = self.el
+    let $host = $(host)
+    $host.append(`<a-box class="color-tweaker-bounds" opacity="0.1" color="white"></a-box>`)
+    $host.append(`<a-sphere class="color-tweaker-bounds" opacity="0.1" color="white"></a-sphere>`)
+    $host.append(`<a-cylinder position="1.2 0 0" rotation="0 0 90" radius="0.5" height="0.3" color="red">`)
+    $host.append(`<a-cylinder position="0 1.2 0" rotation="0 0 0" radius="0.5" height="0.3" color="green">`)
+    $host.append(`<a-cylinder position="0 0 1.2" rotation="90 0 0" radius="0.5" height="0.3" color="blue">`)
+    self.tick = () => {
+      host.setAttribute('color', colorFromEntityRotation(host))
+    }
   }
 });
 
