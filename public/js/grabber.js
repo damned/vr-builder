a20c537ad105917044a80e93539748bba871c39e
Glitch (vr-builder) <none>
2020-07-03 21:00:32 +0000

    💨👣 Checkpoint ./public/js/grabber.js:69342/1764

--- public/js/grabber.js
+++ public/js/grabber.js
@@ -29,17 +29,25 @@ let currentlyTouching = (hand) => {
 }
 
 function groupUnder(groupRoot, child) {
-  child.object3D.updateMatrixWorld()
-  let matrix = new THREE.Matrix4().copy(child.object3D.matrixWorld)
-  clog('group', 'adding', child, 'to', groupRoot)
-  clog('group', 'child', child)
-  clog('group', 'parent', groupRoot)
-  groupRoot.setAttribute('opacity', '0.2')
-  let cloned = cloneEntity(child, false, groupRoot)
-  cloned.matrix.copy(matrix)
-  cloned.applyMatrix(new THREE.Matrix4().getInverse(groupRoot.object3D.parent.matrixWorld))
-  child.parentElement.remove(child) // REMOVE OLD CHILD
-  clog('group', 'reparented')
+  catching(() => {
+    child.object3D.updateMatrixWorld()
+    groupRoot.object3D.updateMatrixWorld()
+    let worldToLocal = new THREE.Matrix4().getInverse(groupRoot.object3D.matrixWorld)
+    
+    
+    let matrix = new THREE.Matrix4().copy(child.object3D.matrixWorld)
+    clog('group', 'adding', child, 'to', groupRoot)
+    clog('group', 'child', child)
+    clog('group', 'parent', groupRoot)
+    groupRoot.setAttribute('opacity', '0.2')
+
+    let cloned = child.cloneNode()
+    cloned.object3D.matrix.copy(matrix)
+    cloned.object3D.applyMatrix4(worldToLocal)
+
+    child.parentElement.remove(child) // REMOVE OLD CHILD
+    clog('group', 'reparented')    
+  })
 }
 
 // debugColor
