f50bf62be143b04108b070cc527716f71c232ae0
Glitch (vr-builder) <none>
2020-07-04 14:10:13 +0000

    🌇🎻 Checkpoint ./public/js/tests/test-transforms.js:69342/10 ./public/tests.html:69342/328 ./public/js/tests/test-transform-understanding.js:69342/331

--- public/js/tests/test-transform-understanding.js
+++ public/js/tests/test-transform-understanding.js
@@ -14,12 +14,14 @@ describe('a-frame and three.js nested entities and transforms', () => {
   describe('a boring single entity in default placement', () => {
     let boring
     let boring3d
-    beforeEach(() => {
+    beforeEach((done) => {
       boring = $('<a-box></a-box>').appendTo($scene).get(0)
+      
       boring3d = boring.object3d
     })
     it('will have local position at origin', () => {
       expect(boring.getAttribute('position')).to.shallowDeepEqual({x: 0, y: 0, z: 0})
+      expect(boring3d.position).to.deep.equal({x: 0, y: 0, z: 0})
     })
   })
 })
