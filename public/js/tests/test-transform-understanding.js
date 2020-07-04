bf57d978893e7685cce85d935ff745b7146dfddb
Glitch (vr-builder) <none>
2020-07-04 14:34:13 +0000

    🛫🚉 Checkpoint ./public/js/tests/test-transform-understanding.js:69342/2467 ./public/tests.html:69342/282

--- public/js/tests/test-transform-understanding.js
+++ public/js/tests/test-transform-understanding.js
@@ -19,29 +19,37 @@ describe('a-frame and three.js nested entities and transforms', () => {
       boring.addEventListener('loaded', () => done())
     })
     
-    it('will have local position at origin', () => {
+    it('will have local a-frame position at origin', () => {
       expect(boring.getAttribute('position'))
         .to.shallowDeepEqual({x: 0, y: 0, z: 0})
-      
+    })
+
+    it('will have local three.js position at origin', () => {
       expect(boring.object3D.position)
         .to.shallowDeepEqual({x: 0, y: 0, z: 0})
     })
-  })
 
-  describe('a single entity positioned somw', () => {
-    let boring
+    it('will have an identity local matrix: one that transforms a point to itself or ', () => {
+      expect(boring.object3D.position)
+        .to.shallowDeepEqual({x: 0, y: 0, z: 0})
+    })
+})
+
+  describe('a single entity translatedsomewhere', () => {
+    let translated
     
     beforeEach((done) => {
-      boring = $('<a-box></a-box>').appendTo($scene).get(0)
-      boring.addEventListener('loaded', () => done())
+      translated = $('<a-box position="1 2 3"></a-box>').appendTo($scene).get(0)
+      translated.addEventListener('loaded', () => done())
     })
     
-    it('will have local position at origin', () => {
-      expect(boring.getAttribute('position'))
-        .to.shallowDeepEqual({x: 0, y: 0, z: 0})
-      
-      expect(boring.object3D.position)
-        .to.shallowDeepEqual({x: 0, y: 0, z: 0})
+    it('will have offset local a-frame position', () => {
+      expect(translated.getAttribute('position'))
+        .to.shallowDeepEqual({x: 1, y: 2, z: 3})
+    })
+    it('will have offset local three.js position', () => {
+      expect(translated.object3D.position)
+        .to.shallowDeepEqual({x: 1, y: 2, z: 3})
     })
   })
 })
