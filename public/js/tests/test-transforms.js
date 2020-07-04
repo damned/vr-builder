cbf607900354b5398838dada0f0b171b1860537a
Glitch (vr-builder) <none>
2020-07-04 13:47:03 +0000

    🍂🍉 Checkpoint ./public/js/tests/test-transforms.js:69342/1004 ./public/tests.html:69342/164

--- public/js/tests/test-transforms.js
+++ public/js/tests/test-transforms.js
@@ -3,20 +3,18 @@ var chai = chai || {}
 var expect = chai.expect
 
 describe('a-frame and three.js nested entities and transforms', () => {
-  let $scene = $('<a-scene>').appendTo('body')
-  
+  let $scene
+  before(() => $scene = $('<a-scene embedded>').appendTo('body'))
   beforeEach(() => $scene.empty())
-  
-  describe('a boring single entity in default placement', () => {
+  xdescribe('a boring single entity in default placement', () => {
     let boring
+    let boring3d
     beforeEach(() => {
       boring = $('<a-box></a-box>').appendTo($scene).get(0)
-      })
-    it('will add a property if it does not exist', () => {
-      let original = '<a-box></a-box>'
-      expect(patchHtml(original, { 
-        rotation: 'fubar'
-      })).to.eql('<a-box rotation="fubar"></a-box>')
+      boring3d = boring.object3d
+    })
+    it('will have local position at origin', () => {
+      expect(boring.getAttribute('position')).to.eql({x: 0, y: 0, z: 0})
     })
   })
 })
