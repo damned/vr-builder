15030724df899872a499ebc31dab1096b30835b7
Glitch (vr-builder) <none>
2020-07-04 14:21:40 +0000

    nice testing of basic aframe and three.js behaviour

--- public/js/tests/test-transform-understanding.js
+++ public/js/tests/test-transform-understanding.js
@@ -3,7 +3,7 @@ var chai = chai || {}
 var expect = chai.expect
 
 describe('a-frame and three.js nested entities and transforms', () => {
-  let $scene = $('<a-scene embedded>').appendTo('body')
+  let $scene = $('<a-scene style="height: 200px" embedded>').appendTo('body')
 
   beforeEach(() => $scene.empty())
   after(() => {
@@ -13,15 +13,18 @@ describe('a-frame and three.js nested entities and transforms', () => {
   
   describe('a boring single entity in default placement', () => {
     let boring
-    let boring3d
+    
     beforeEach((done) => {
       boring = $('<a-box></a-box>').appendTo($scene).get(0)
-      
-      boring3d = boring.object3d
+      boring.addEventListener('loaded', () => done())
     })
+    
     it('will have local position at origin', () => {
-      expect(boring.getAttribute('position')).to.shallowDeepEqual({x: 0, y: 0, z: 0})
-      expect(boring3d.position).to.deep.equal({x: 0, y: 0, z: 0})
+      expect(boring.getAttribute('position'))
+        .to.shallowDeepEqual({x: 0, y: 0, z: 0})
+      
+      expect(boring.object3D.position)
+        .to.shallowDeepEqual({x: 0, y: 0, z: 0})
     })
   })
 })
