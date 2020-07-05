584252b820779993be8d58989761e31873ff6db5
Glitch (vr-builder) <none>
2020-07-05 14:47:33 +0000

    💎🏭 Checkpoint ./public/index.html:69342/163 ./public/js/letterbox.js:69342/826

--- public/js/letterbox.js
+++ public/js/letterbox.js
@@ -10,10 +10,15 @@ AFRAME.registerComponent('letterbox', {
       key = self.data
       $(`<a-text align="center" wrap-count="100" position="0 0 0.051" scale"0.2 0.2 0.2" baseline="center" value="${key}"></a-text>`).appendTo(host)
     }
+    let $keyed = $("#keyed")
     host.classList.add('letterbox')
     host.addEventListener('hitstart', (event) => {
       clog('letterbox', event.target)
       clog('letterbox', 'key: ' + key)
+      let logValue = $keyed.attr('value')
+      logValue += key
+      $keyed.attr("value", logValue);
+
       host.setAttribute('color', 'white')
       setTimeout(() => host.setAttribute('color', 'gray'), 100)         
     })
