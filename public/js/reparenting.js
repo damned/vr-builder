5f3776c5b208785decca686c4f785e8fe9ed27ff
Glitch (vr-builder) <none>
2020-07-05 12:37:29 +0000

    🚍🎇 Checkpoint ./public/js/reparenting.js:69342/3254

--- public/js/reparenting.js
+++ public/js/reparenting.js
@@ -11,21 +11,21 @@ function reparent(entity, newParent, done) {
   newParent.appendChild(reparented)
 
   reparented.addEventListener('loaded', () => {
-    // based on parent-world.local matrix multiplication order for child's world matrix:
-    //   https://github.com/mrdoob/three.js/blob/dev/src/core/Object3D.js#L560
-    //
-    // determine required child local matrix thus:
-    //   https://math.stackexchange.com/questions/949341/how-to-find-matrix-b-given-matrix-ab-and-a          
-    let recalculatedLocalMatrix = new THREE.Matrix4().getInverse(newParent.object3D.matrixWorld).multiply(intendedWorldMatrix)
-
-    let matrixAutoUpdate = reparented.object3D.matrixAutoUpdate
-    reparented.object3D.matrixAutoUpdate = false // so that local matrix doesn't get trashed on tick while updating 
-    reparented.object3D.matrix.copy(recalculatedLocalMatrix)
-    reparented.object3D.applyMatrix(identityMatrix) // to set pos, rot, scale from matrix
-    reparented.object3D.matrixAutoUpdate = matrixAutoUpdate
-    newParent.object3D.updateMatrixWorld() // ensure all world matrices around moved entity are consistent
-
     done()
   })
+  // based on parent-world.local matrix multiplication order for child's world matrix:
+  //   https://github.com/mrdoob/three.js/blob/dev/src/core/Object3D.js#L560
+  //
+  // determine required child local matrix thus:
+  //   https://math.stackexchange.com/questions/949341/how-to-find-matrix-b-given-matrix-ab-and-a          
+  let recalculatedLocalMatrix = new THREE.Matrix4().getInverse(newParent.object3D.matrixWorld).multiply(intendedWorldMatrix)
+
+  let matrixAutoUpdate = reparented.object3D.matrixAutoUpdate
+  reparented.object3D.matrixAutoUpdate = false // so that local matrix doesn't get trashed on tick while updating 
+  reparented.object3D.matrix.copy(recalculatedLocalMatrix)
+  reparented.object3D.applyMatrix(identityMatrix) // to set pos, rot, scale from matrix
+  reparented.object3D.matrixAutoUpdate = matrixAutoUpdate
+  newParent.object3D.updateMatrixWorld() // ensure all world matrices around moved entity are consistent
+
   return reparented
 }
