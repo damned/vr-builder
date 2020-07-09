d02b0d06aa538680e48732ddaa7ea84c13129d8d
Glitch (vr-builder) <none>
2020-07-09 14:31:39 +0000

    🎯🔖 Checkpoint ./public/js/keyboard.js:69342/809

--- public/js/keyboard.js
+++ public/js/keyboard.js
@@ -4,6 +4,13 @@ AFRAME.registerComponent('keyboard', {
   init: function() {
     let self = this
     let host = self.el
-    host.addEventListener('keydown')
+    host.addEventListener('keydown', (event) => {
+      clog('keyboard', 'keydown event target', event.target)
+      clog('keyboard', 'keydown event detail', event.detail)
+    })
+    host.addEventListener('keyup', (event) => {
+      clog('keyboard', 'keyup event target', event.target)
+      clog('keyboard', 'keyup event detail', event.detail)
+    })
   }
 })
