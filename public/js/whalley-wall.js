1463cc7d246ade9ffab57b5c72aa3a0366d27a03
Glitch (vr-builder) <none>
2020-07-11 22:06:01 +0000

    🎺🕷 Checkpoint ./public/js/whalley-wall.js:69342/2564 ./public/index.html:69342/9

--- public/js/whalley-wall.js
+++ public/js/whalley-wall.js
@@ -1,6 +1,8 @@
 /* global AFRAME whalley */
-
 let VrCardViewFactory = function() {
+  let VrCardView = function() {
+    
+  }
   return {
     create_card_view: function(vrWall, card) {
       
@@ -37,7 +39,23 @@ let VrWall = function(logical_wall) {
   }
   var cards_api = CardsApi(logical_wall.cards_api);
 
+  function build(builder) {
+    // svg.clear();
+    cards_api.clear();
+    builder(cards_api);
+    // shelf.reload();
+  }
+
   let external = {
+    build: build,
+    
+    cards: function() {
+      return logical_wall.cards.map(function(card) { return card.data() })
+    },
+    wall_data: function(data) {
+      return logical_wall.wall_data(data)
+    },
+
     move_card: logical_wall.move_card,
     add_card: cards_api.add,
 
@@ -51,18 +69,22 @@ let VrWall = function(logical_wall) {
 AFRAME.registerComponent('whalley-wall', {
   schema: {type: 'string', default: ''},
   init: function() {
-  },
-  self.update = function(oldData) {
-    let wallId = this.data
-    let logical_wall = new whalley.LogicalWall()
-    let visible_wall = VrWall(logical_wall);
-    let sync_client = whalley.SocketIoSyncClient(visible_wall); // NB tied to visible wall at mo because visible wall needs to create view
-                                                                // when it forwards card add to logical wall
-                                                                // could tie directly to logical wall if made wall view create card view
-                                                                // in response to "card created" event
-    sync_client.load_wall(wallId, function() {
-      
-    });
+    let self = this
+    let host =self.el
+    
+    whalley.host = 'https://damned-whalley.glitch.me'
 
+    self.update = function(oldData) {
+      let wallId = self.data
+      let logical_wall = new whalley.LogicalWall()
+      let visible_wall = VrWall(logical_wall);
+      let sync_client = whalley.SocketIoSyncClient(visible_wall); // NB tied to visible wall at mo because visible wall needs to create view
+                                                                  // when it forwards card add to logical wall
+                                                                  // could tie directly to logical wall if made wall view create card view
+                                                                  // in response to "card created" event
+      sync_client.load_wall(wallId, () => {
+        host.setAttribute('color', 'white')
+      });
+    }
   }
 });
