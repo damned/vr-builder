9b084ccab634e3d8937c8a75658e6ea3d5e2542a
Glitch (vr-builder) <none>
2020-07-11 21:43:42 +0000

    👫✨ Checkpoint ./public/index.html:69342/609 ./public/js/whalley-wall.js:69342/1848

--- public/js/whalley-wall.js
+++ public/js/whalley-wall.js
@@ -1,7 +1,50 @@
 /* global AFRAME whalley */
 
-let VrWall = function(logical) {
+let VrCardViewFactory = function() {
+  return {
+    create_card_view: function(vrWall, card) {
+      
+    }
+  }
+}
+
+let VrWall = function(logical_wall) {
+  let self = this
+  var wall_view_api = {
+    create_card_view: function(card) {
+      return VrCardViewFactory().create_card_view(self, card)
+    },
+    group: function(width, height) {
+      console.warn('VrWall', 'group - NYI - doing nothing')
+      return {}
+    },
+    show_card: function(card) {
+      console.warn('VrWall', 'show_card - NYI - doing nothing')
+    }
+  }
+
   
+  function CardsApi(logical_cards_api) {
+    return {
+      add: function (cardlike) {
+        var logical_card = logical_cards_api.add(cardlike);
+        wall_view_api.create_card_view(logical_card);
+        return logical_card;
+      },
+      clear: logical_cards_api.clear
+    }
+  }
+  var cards_api = CardsApi(logical_wall.cards_api);
+
+  let external = {
+    move_card: logical_wall.move_card,
+    add_card: cards_api.add,
+
+    on_card_add: logical_wall.on_card_add,
+    on_card_moving: logical_wall.on_card_moving,
+    on_card_changed: logical_wall.on_card_changed
+  }
+  return external
 }
 
 AFRAME.registerComponent('whalley-wall', {
