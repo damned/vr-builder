a551c2fbba93959c585639173c9ee5c521722289
Glitch (vr-builder) <none>
2020-07-15 14:42:12 +0000

    🐙🎟 Checkpoint ./public/js/move-handles.js:69342/306 ./public/index.html:69342/16

--- public/js/move-handles.js
+++ public/js/move-handles.js
@@ -2,7 +2,8 @@
 AFRAME.registerComponent('move-handles', {
   schema: {
     extent: {type: 'number', default: 5 },
-    gap: {type: 'number', default: 1 }
+    gap: {type: 'number', default: 1 },
+    height: {type: 'number', default: 1 }
   },
   init: function() {
     let self = this
@@ -12,18 +13,21 @@ AFRAME.registerComponent('move-handles', {
     let extent
     let gap
     let offset
+    let height
     
     self.update = () => {
       extent = self.data.extent
       gap = self.data.gap
+      height = self.data.height
       offset = -Math.floor(extent / 2) * gap
         
+      // todo: move some of this to move-handle
       handleCount = extent * extent
       for (let i = 0; i < handleCount; i++) {
-        $('<a-box opacity="0.2" mixin="movehandle"></a-box>').appendTo(host)
+        $('<a-box><a-image user-move-handle opacity="0.15" rotation="90 0 0" width="0.2" height="0.2" src="#four-way-arrow"></a-image><a-box>').appendTo(host)
       }
       host.setAttribute('layout', `type: box; columns: ${extent}; margin: ${gap}; plane: xz`)
-      host.setAttribute('position', `${offset} 1 ${offset}`)
+      host.setAttribute('position', `${offset} ${height} ${offset}`)
     }
   },
 })
