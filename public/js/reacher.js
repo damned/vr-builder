27ea54ef7ec70ac2c88d961e4548c657ceb4656c
Glitch (vr-builder) <none>
2020-07-16 10:57:51 +0000

    ⛩👷 Checkpoint ./public/js/reacher.js:69342/509

--- public/js/reacher.js
+++ public/js/reacher.js
@@ -3,14 +3,16 @@
 AFRAME.registerComponent('reacher', {
   schema: {
     componentsToPlay: {type: 'array'},
-    distance: {type: 'number', default: 0.8 }
+    distance: {type: 'number', default: 0.8 },
+    fromPointHeight: {type: 'number', default: 1 }
   },
 
   init: function() {
     let self = this
     let host = self.el
     let host3d = host.object3D
-    let fromPoint = new THREE.Vector3()
+    let fromPointHeight
+    let fromElement3d = host.sceneEl.camera
     
     let componentsToPlay
     let reachDistance
@@ -19,17 +21,20 @@ AFRAME.registerComponent('reacher', {
     self.update(() => {
       componentsToPlay = self.data.componentsToPlay
       reachDistance = self.data.distance
+       
+      fromPointHeight = self.data.fromPointHeight
     })
     let tickHandler = () => {
       catching(() => {
+        let fromPoint = fromElement3d.position
         let distance = fromPoint.distanceTo(host3d.position)
         clog('reacher', distance)
         if (!reaching && distance > reachDistance) {
-          clog('reach!!!!!')
+          clog('reacher', 'reach!!!!!')
           reaching = true
         }
         if (reaching && distance < reachDistance) {
-          clog('unreach!!!!!')
+          clog('reacher', 'unreach!!!!!')
           reaching = false
         }        
       })
