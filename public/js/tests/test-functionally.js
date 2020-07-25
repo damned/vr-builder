cc829245270d7a9e5ecd21754d462653e144872a
Glitch (vr-builder) <none>
2020-07-25 15:33:20 +0000

    🍼🚞 Checkpoint ./public/js/tests/test-functionally.js:69342/308

--- public/js/tests/test-functionally.js
+++ public/js/tests/test-functionally.js
@@ -5,16 +5,15 @@ var expect = chai.expect
 describe('functional testing with aframe', () => {
   
   it('load up aframe', (done) => {
-    let scene = '<a-scene embedded>' 
+    console.log('loading up aframe')
+    let scene = '<a-scene embedded style="height: 100px; width: 300px;">' 
       +   '<a-box color="yellow" position="0 0 -1"></a-box>'
       + '</a-scene>'
     let $scene = $(scene)
     $scene.appendTo($('#aframe-container'))
-    $scene.on('loaded', () => {
-    })
-    setTimeout(() => {
+    $scene.on('renderstart', () => {
       expect($scene.get(0).renderStarted).to.eql(true)
-      done()    
-    }, 5000)
+      done()
+    })
   })
 })
