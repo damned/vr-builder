018219621626f347bd75a8dae25cc39316efc9a8
Glitch (vr-builder) <none>
2020-07-25 15:21:19 +0000

    🐈👩‍👩‍👦 Checkpoint ./public/js/tests/test-functionally.js:69342/679

--- public/js/tests/test-functionally.js
+++ public/js/tests/test-functionally.js
@@ -3,10 +3,18 @@ var chai = chai || {}
 var expect = chai.expect
 
 describe('functional testing with aframe', () => {
-  it('load up aframe', () => {
-    let scene = '<a-scene>' 
-      +   '<a-box position="4 0 1"></a-box>'
+  
+  it('load up aframe', (done) => {
+    let scene = '<a-scene embedded>' 
+      +   '<a-box color="yellow" position="0 0 -1"></a-box>'
       + '</a-scene>'
-    expect(patchHtml(original, { position: 'bob' })).to.eql('<a-box position="bob"></a-box>')
+    let $scene = $(scene)
+    $scene.appendTo($('#aframe-container'))
+    $scene.on('loaded', () => {
+    })
+    setTimeout(() => {
+      expect($scene.get(0).renderStarted).to.eql(true)
+      done()    
+    }, 5000)
   })
 })
