ff2c24c3c67e57eda0cc4220c66cb7ddde125a81
Glitch (vr-builder) <none>
2020-07-25 16:08:19 +0000

    📉✈️ Checkpoint ./public/js/tests/scene/test-mini-scenes.js:69342/1594 ./public/tests.html:69342/43

--- public/js/tests/scene/test-mini-scenes.js
+++ public/js/tests/scene/test-mini-scenes.js
@@ -4,16 +4,53 @@ var expect = chai.expect
 
 describe('functional testing with aframe', () => {
   
+  function createSceneFixture(done) {
+    console.log('loading up aframe')
+    let sceneHtml = '<a-scene embedded style="height: 400px; width: 600px;">' 
+      +   '<a-box color="gray" position="0 1 -2"></a-box>'
+      + '</a-scene>'
+    let $scene = $(sceneHtml)
+    $scene.appendTo($('#aframe-container'))
+
+    let scene = $scene.get(0)
+    
+    let startupHandler() => {
+      expect(scene.renderStarted).to.eql(true)
+      scene.pause()
+      $scene.remove()
+      scene = null
+      $scene = null
+      done()
+    }
+    let startHandlers = []
+    $scene.on('renderstart', )
+    return {
+      $scene: $scene,
+      scene: scene,
+      cleanUp: () => {
+        
+      },
+      onStart: (handler) => {
+        startHandlers.
+      }
+    }
+  }
+  
   for (let i=0; i < 20; i++) {
     it('load up aframe: ' + i, (done) => {
       console.log('loading up aframe')
-      let scene = '<a-scene embedded style="height: 400px; width: 600px;">' 
+      let sceneHtml = '<a-scene embedded style="height: 400px; width: 600px;">' 
         +   '<a-box color="gray" position="0 1 -2"></a-box>'
         + '</a-scene>'
-      let $scene = $(scene)
+      let $scene = $(sceneHtml)
       $scene.appendTo($('#aframe-container'))
+      let scene = $scene.get(0)
       $scene.on('renderstart', () => {
-        expect($scene.get(0).renderStarted).to.eql(true)
+        expect(scene.renderStarted).to.eql(true)
+        scene.pause()
+        $scene.remove()
+        scene = null
+        $scene = null
         done()
       })
     })
