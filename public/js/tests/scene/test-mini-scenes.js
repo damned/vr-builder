18c8711e6e2b59b0075e4f24e5ca63bf18998f04
Glitch (vr-builder) <none>
2020-07-25 17:52:47 +0000

    split out scene fixture to support file; cleaned logging - quite rapid

--- public/js/tests/scene/test-mini-scenes.js
+++ public/js/tests/scene/test-mini-scenes.js
@@ -1,65 +1,8 @@
-/* global AFRAME */
+/* global AFRAME createSceneFixture */
 var chai = chai || {}
 var expect = chai.expect
 
 describe('functional testing with aframe', function() {
-  function createSceneFixture(options) {
-    console.log('loading up aframe')
-    options = options || { stats: true }
-    let statsSpec = options.stats ? 'stats' : ''
-    let sceneHtml = `<a-scene ${statsSpec} embedded style="height: 400px; width: 600px;"></a-scene>`
-    let $scene = $(sceneHtml)
-    $scene.appendTo($('#aframe-container'))
-
-    let scene = $scene.get(0)
-    let startHandler = null
-    
-    let isAframeInternalEntity = entity => entity.hasAttribute('aframe-injected')
-    
-    let getSceneEntities = () => {
-      return Array.from(scene.object3D.children)
-        .filter(child => (child.el && !isAframeInternalEntity(child.el)))
-        .map(child => child.el)
-    }
-
-    let cleanUp = () => {
-      if (startHandler) {
-        scene.removeEventListener('renderstart', onRenderStartHandler)
-        startHandler = null
-      }
-      getSceneEntities().forEach(entity => {
-        console.log('removing', entity, entity.tagName)
-        scene.removeChild(entity)
-      })
-    }
-        
-    let onRenderStartHandler = () => {
-      if (startHandler) {
-        startHandler()
-      }
-    }    
-    return {
-      $scene: $scene,
-      scene: scene,
-      append: (html) => {
-        $scene.append(html)
-      },
-      cleanUp: cleanUp,
-      apply: (handler) => {
-        if (scene.renderStarted) {
-          console.log('render already started!')
-          setTimeout(() => {
-            handler()
-            cleanUp()
-          }, 0)
-        }
-        else {
-          startHandler = handler
-          scene.addEventListener('renderstart', onRenderStartHandler)
-        }
-      }
-    }
-  }
   const colors = ['yellow', 'red', 'blue', 'green', 'lightyellow', 'pink', 'lightgreen', 'white']
   
   let scene = createSceneFixture({stats: false})
